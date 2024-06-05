a34cf18f2dc598977d50f7220df27fa173777f6a
defected07 <defected07@proton.me>
2024-06-04 20:08:06 -0700

    👽️ Fixed script to work with modified RSS feed. Added too many debug statements for now -- will add flag to print verbose output

--- scrape.py
+++ scrape.py
@@ -3,40 +3,71 @@
 import argparse
 import itertools
 import json
-import xml.etree.ElementTree
+import xmltodict
+from xml.etree.ElementTree import fromstring, tostring
 
 import requests
 
+APPCAST_URL = 'https://desktop.docker.com/mac/main/{arch}/appcast.xml'
+DOWNLOAD_URL = 'https://desktop.docker.com/mac/main/{arch}/{build}/Docker.dmg'
+SPARKLE_NS = '{http://www.andymatuschak.org/xml-namespaces/sparkle}'
 
-APPCAST_URL = 'https://download.docker.com/mac/{channel}/appcast.xml'
-DOWNLOAD_URL = 'https://download.docker.com/mac/{channel}/{build}/Docker.dmg'
-
-
-def get_latest_build_number(channel):
+def get_latest_build_number(arch):
     """
     Parse the appcast XML to find the Build number of the latest release.
     """
-    url = APPCAST_URL.format(channel=channel)
-    resp = requests.get(url)
+    arch_url = APPCAST_URL.format(arch=arch)
+    print('\nURL = {}\n'.format(arch_url))
+    resp = requests.get(arch_url)
+    print('\nResponse Text = \n{}\n'.format(resp.text))
 
-    ns = {'sparkle': '{http://www.andymatuschak.org/xml-namespaces/sparkle}'}
-    root = xml.etree.ElementTree.fromstring(resp.text)
+    ns = {'sparkle': SPARKLE_NS}
+    print('\nns = {}\n'.format(ns))
 
-    for enclosure in root.findall('.//enclosure', ns):
-        version_attrib = '{ns}version'.format(ns=ns['sparkle'])
+    root = fromstring(resp.text)
+    root_attrib = root.attrib
+    print('\nRoot Attrib = {}\n'.format(root_attrib))
+    root_keys = root.keys()
+    print('\nRoot Keys = {}\n'.format(root_keys))
+    root_items = root.items()
+    print('\nRoot Items = {}\n'.format(root_items))
+
+    enclosures = root.findall('.//enclosure', ns)
+    enclosures_length = 0 or len(enclosures)
+    print('\nEnclosures Length = {}\n'.format(enclosures_length))
+
+    for (idx, enclosure) in enumerate(enclosures):
+        print('\nEnclosure Keys = {}\n'.format(enclosure.keys()))
+        print('\nEnclosure Items = {}\n'.format(enclosure.items()))
+        print('\nEnclosure JSON = {}\n'.format(xmltodict.parse(tostring(enclosure))))
+
+        url_attrib = 'url'.format(ns=ns['sparkle'])
+        print('\nEnclosure URL Attribute: {}\n'.format(url_attrib))
+        url = enclosure.attrib[url_attrib]
+        print('\nEnclosure URL: {}\n'.format(url))
+
+        previous_build_attrib = 'previousBuild'.format(ns=ns['sparkle'])
+        print('\nEnclosure Prev Build Attribute: {}\n'.format(previous_build_attrib))
+        previous_build = enclosure.attrib[previous_build_attrib]
+        print('\nEnclosure Previous Build: {}\n'.format(previous_build))
+
+        version_attrib = '{}version'.format(SPARKLE_NS, ns=ns['sparkle'])
+        print('\nEnclosure Version Attribute: {}\n'.format(version_attrib))
         version = enclosure.attrib[version_attrib]
+        print('\nEnclosure Version: {}\n'.format(version))
+
         return int(version)
     return None
 
 
-def scan(channel, latest_build, limit=None):
+def scan(arch, latest_build, limit=None):
     build_limit = latest_build - limit if limit else 0
 
     builds = []
     for build in range(latest_build, build_limit, -1):
         try:
             resp = requests.head(DOWNLOAD_URL.format(
-                channel=channel,
+                arch=arch,
                 build=build
             ))
             resp.raise_for_status()
@@ -44,6 +75,8 @@ def scan(channel, latest_build, limit=None):
             continue
 
         headers = resp.headers
+        print('\nArch = {}\n\nBuild = {}\n\nHeaders = {}\n'.format(arch, build, headers))
+
         headers['url'] = resp.url
         builds.append(dict(headers))
 
@@ -54,22 +87,27 @@ def main():
     parser = argparse.ArgumentParser(
         description='Scrape for Docker for Mac release download links'
     )
-    parser.add_argument('channel', choices=['stable', 'edge'])
+    parser.add_argument('arch', choices=['amd64', 'arm64'])
     parser.add_argument('--limit', type=int,
                         help='Maximum number of builds to scan for')
     parser.add_argument('--out', type=str,
                         help='JSON file to output build info')
     args = parser.parse_args()
+    print('Args = {}.'.format(args))
 
     if args.out is None:
-        args.out = args.channel + '.json'
+        args.out = args.arch + '.json'
 
-    latest_build = get_latest_build_number(args.channel)
-    builds = scan(args.channel, latest_build, limit=args.limit)
+    latest_build = get_latest_build_number(args.arch)
+    print('\nLatest Build = {}\n'.format(latest_build))
+
+    builds = scan(args.arch, latest_build, limit=args.limit)
+    print('\nBuilds = {}\n'.format(builds))
 
     with open(args.out, 'w') as f:
         json.dump(builds, f)
-    print('Wrote {}.'.format(args.out))
+
+    print('\nWrote {}\n'.format(args.out))
 
 
 if __name__ == '__main__':
